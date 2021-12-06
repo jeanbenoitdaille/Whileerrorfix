5e1c6f3b617f0928d8994f5581352dc72bd167be
jean-benoit daille <68898672+jeanbenoitdaille@users.noreply.github.com>
2021-12-06 12:21:19 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function errorLoop() {
+    for (var i = 0; i < 10; i++) { // Modifiez seulement cette ligne
+        /* ... */
+    }
+      
+    // Ne modifiez pas cette ligne  
+    console.log(i);
+}
